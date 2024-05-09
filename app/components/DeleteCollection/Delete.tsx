6a35967c97717891657bcb1c21d6284e63d156aa
Joey <127422383+JLWebDeV23@users.noreply.github.com>
2024-05-09 15:12:19 +1000

    Refactor DeleteCollection component to improve collection deletion process

--- app/components/DeleteCollection/Delete.tsx
+++ app/components/DeleteCollection/Delete.tsx
@@ -6,31 +6,43 @@ type DeleteProps = {
 };
 
 const Delete: React.FC<DeleteProps> = ({ client }) => {
-    const deleteCollection = async () => {
-        // const name = prompt("Enter the name of the collection to delete");
-        const name = "test";
-        const response = await client.getCollections();
-        const collectionNames = response.collections.map(
-          (collection) => collection.name
-        );
-        try {
-          if (collectionNames.includes(name)) {
-            const delete_status = await client.deleteCollection(name);
-            console.log(delete_status);
-          }
-        } catch (error) {
-          console.error("Error deleting collection:", error);
-        }
-      };
-    
-      return (
-        <button
-          className="delete-btn mt-5 border p-2 rounded text-white bg-red-500 hover:bg-red-600"
-          onClick={deleteCollection}
-        >
-          Delete
-        </button>
-      );
+  const getCollectionsList = async () => {
+    const response = await client.getCollections();
+    return response.collections;
+  };
+
+  const deleteCollection = async () => {
+    // const name = prompt("Enter the name of the collection to delete");
+    const name = prompt(`Enter the name of the collection to delete:
+- - -
+${(await getCollectionsList())
+  .map((collection) => collection.name)
+  .join("\n")}
+- - -
+`);
+    const response = await client.getCollections();
+    const collectionNames = response.collections.map(
+      (collection) => collection.name
+    );
+    try {
+      if (collectionNames.includes(name!)) {
+        const delete_status = await client.deleteCollection(name!);
+        alert(`Collection ${name} deleted successfully!`);
+        location.reload();
+      }
+    } catch (error) {
+      console.error("Error deleting collection:", error);
+    }
+  };
+
+  return (
+    <button
+      className="delete-btn mt-5 border p-2 rounded text-white bg-red-500 hover:bg-red-600"
+      onClick={deleteCollection}
+    >
+      Delete
+    </button>
+  );
 };
 
 export default Delete;
