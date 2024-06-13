b111a6ceb4bc83df2e7c7b22d5d1729274f0acbb
Joey <127422383+JLWebDeV23@users.noreply.github.com>
2024-06-13 15:33:56 +1000

    refactor: Add testData.js

---
+++ app/version_2_0/TestData/testData.js
@@ -0,0 +1,170 @@
+export const testData = [
+  [
+    {
+      model: "OpenAI",
+      subModel: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is Joey",
+        },
+      ],
+      locked: true,
+    },
+    {
+      model: "LlaMA 3",
+      subModel: "llama3-8b-8192",
+      messages: [
+        {
+          role: "system",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is Joey",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is Joey",
+        },
+      ],
+      locked: true,
+    },
+  ],
+  [
+    {
+      model: "OpenAI",
+      subModel: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is alice",
+        },
+      ],
+      locked: true,
+    },
+    {
+      model: "LlaMA 3",
+      subModel: "llama3-8b-8192",
+      messages: [
+        {
+          role: "system",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is alice",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is alice",
+        },
+      ],
+      locked: true,
+    },
+  ],
+  [
+    {
+      model: "OpenAI",
+      subModel: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is John",
+        },
+      ],
+      locked: true,
+    },
+    {
+      model: "LlaMA 3",
+      subModel: "llama3-8b-8192",
+      messages: [
+        {
+          role: "system",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "User",
+          content: "Hello, my name is John",
+        },
+        {
+          role: "Assistant",
+          content: "Hello, my name is John",
+        },
+      ],
+      locked: true,
+    },
+  ],
+];
