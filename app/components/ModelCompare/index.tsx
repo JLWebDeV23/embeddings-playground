4e87afa56fe9eafb6fa3230359c3667cbec0697f
Mathias ROSA <mat.ros@outlook.fr>
2024-06-14 12:02:11 +1000

    chose model

--- app/components/ModelCompare/index.tsx
+++ app/components/ModelCompare/index.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import InputBox from '@/app/components/InputBox/InputBox';
-import { Select, SelectItem } from "@nextui-org/react";
+import { Button, Select, SelectItem, SelectSection } from "@nextui-org/react";
 import models from "@/public/assets/data/ModelData.json";
+import { IonIcon } from '@ionic/react';
 
 
 export default function ModelCompare() {
     const [sysMessage, setSysMessage] = useState<string>("");
+    const [selectedModels, setSelectedModels] = useState<string[]>([]);
 
-    /*     console.log(models.map((model) => model.submodel)) */
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedModels([...selectedModels, e.target.value]);
+    }
 
     const submitSysMessage = () => {
         console.log(sysMessage);
@@ -33,44 +37,32 @@ export default function ModelCompare() {
             </div>
             <div className="divider"></div>
             <div className="flex gap-3 flex-wrap">
+                {
+                    selectedModels.map((model, index) => (
+                        <Button
+                            key={index}
+                            endContent={<IonIcon name='close-outline' />}
+                            onClick={() => alert("not implemented")}
+                        >
+                            {model}
+                        </Button>
+                    ))
+                }
                 <Select
-                    placeholder="Select an animal"
-                    className="max-w-xs"
+                    placeholder="Add a new model"
+                    className="max-w-64"
+                    onChange={handleChange}
+                    selectedKeys={[]}
+                    aria-label="Select a model"
                 >
                     {models.map((model, findex) =>
-                        <SelectItem key={findex}>
-                            {model.model}
-                        </SelectItem>
+                        <SelectSection title={model.model} key={model.model}>
+                            {model.submodel.map((submodel, sindex) =>
+                                <SelectItem key={submodel.model} value={sindex}>{submodel.model}</SelectItem>
+                            )}
+                        </SelectSection>
                     )}
                 </Select>
-                {models.map((model, index) => (
-                    <button
-                        key={index}
-                        className="btn btn-sm"
-                        onClick={() => alert("not implemented")}
-                    >
-                        {model.model}
-                    </button>
-                ))
-                }
-                <button
-                    className="btn btn-sm btn-disabled"
-                    onClick={() => alert("not implemented")}
-                >
-                    LlaMA 3: llama3-8b-8192
-                </button>
-                <button
-                    className="btn btn-sm btn-disabled"
-                    onClick={() => alert("not implemented")}
-                >
-                    LlaMA 3: llama3-8b-8192
-                </button>
-                <button
-                    className="btn btn-sm"
-                    onClick={() => alert("not implemented")}
-                >
-                    Add a new model
-                </button>
             </div>
         </div>
     )
