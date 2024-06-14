200d696bcf2d31f8fb707b44d5eb3e1f7ccfbb1b
Joey <127422383+JLWebDeV23@users.noreply.github.com>
2024-06-14 15:11:19 +1000

    Add manage session storage functions

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,1 +1,5 @@
 export { insertUserPrompt, go, createNewModelData } from "./modelProcessing";
+
+export { addApiKey, removeApiKey, getApiKeys } from "./functions";
+
+//Todo: replace all private api keys with sessions storage keys
