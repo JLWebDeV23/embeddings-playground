caa8e05e26b096ed119643b0d44a7e06e5d85df4
Mathias ROSA <mat.ros@outlook.fr>
2024-06-25 19:24:46 +1000

    api keys in a hook. TODO : find a better way to get the key from the storage when initialising the apiKey state

--- app/hooks/useApiKeys.ts
+++ app/hooks/useApiKeys.ts
@@ -1,82 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ApiKeyProps } from "../utils/interfaces";
 
-export const addApiKeyToStorage = (props: ApiKeyProps) => {
-    const API_KEYS = JSON.parse(sessionStorage.getItem("API_KEYS") || "[]");
-
-    if (props.apiKey === "") {
-        // delete the key if it is empty
-        removeApiKeyFromStorage(props);
-        return;
-    }
-
-    const newApiKeys = [...API_KEYS, props];
-
-    try {
-        sessionStorage.setItem("API_KEYS", JSON.stringify(newApiKeys));
-    } catch (error) {
-        console.error("Add API Key Error:", error);
-    }
-};
-
-export const removeApiKeyFromStorage = (props: ApiKeyProps) => {
-    const { name } = props;
-    console.log("name", name);
-    try {
-        const API_KEYS = JSON.parse(sessionStorage.getItem("API_KEYS") || "[]");
-        const newApiKeys = API_KEYS.filter(
-            (key: ApiKeyProps) => key.name !== name
-        );
-        console.log("newApiKeys", newApiKeys);
-        sessionStorage.setItem("API_KEYS", JSON.stringify(newApiKeys));
-    } catch (error) {
-        console.error("Remove API Key Error:", error);
-    }
-};
-
-export const getApiKeysFromStorage = (): ApiKeyProps[] => {
-    const API_KEYS = JSON.parse(sessionStorage.getItem("API_KEYS") || "[]");
-    return API_KEYS.map((key: ApiKeyProps) => {
-        return {
-            name: key.name,
-            apiKey: key.apiKey,
-        };
-    });
-};
-
-export const getApiKeyFromStorage = (name: string): string | undefined => {
-    const API_KEYS = JSON.parse(sessionStorage.getItem("API_KEYS") || "[]");
-    const key = API_KEYS.find((key: ApiKeyProps) => key.name === name);
-    return key ? key.apiKey : undefined;
-};
-
 export const useApiKeys = () => {
-    const [apiKeys, setApiKeysState] = useState<ApiKeyProps[]>([
+    /* Get the apiKeys from the storage */
+    function getKeyFromStorage(name: string) {
+        const API_KEYS = sessionStorage.getItem("API_KEYS");
+        if (API_KEYS) {
+            const keys = JSON.parse(API_KEYS);
+            const key = keys.find((k: ApiKeyProps) => k.name === name);
+            if (key) {
+                return key.apiKey;
+            }
+        }
+        return "";
+    }
+
+    const [apiKeys, setApiKeysState] = useState<ApiKeyProps[]>(() => [
         {
             name: "NEXT_PUBLIC_OPENAI_API_KEY",
-            apiKey: getApiKeyFromStorage("NEXT_PUBLIC_OPENAI_API_KEY") || "",
+            apiKey: getKeyFromStorage("NEXT_PUBLIC_OPENAI_API_KEY"),
         },
         {
             name: "NEXT_PUBLIC_LLAMA_API_KEY",
-            apiKey: getApiKeyFromStorage("NEXT_PUBLIC_LLAMA_API_KEY") || "",
+            apiKey: getKeyFromStorage("NEXT_PUBLIC_LLAMA_API_KEY"),
         },
         {
             name: "CLAUDE_API_KEY",
-            apiKey: getApiKeyFromStorage("CLAUDE_API_KEY") || "",
+            apiKey: getKeyFromStorage("CLAUDE_API_KEY"),
         },
         {
             name: "QDRANT_API_KEY",
-            apiKey: getApiKeyFromStorage("QDRANT_API_KEY") || "",
+            apiKey: getKeyFromStorage("QDRANT_API_KEY"),
         },
         {
             name: "MISTRAL_API_KEY",
-            apiKey: getApiKeyFromStorage("MISTRAL_API_KEY") || "",
+            apiKey: getKeyFromStorage("MISTRAL_API_KEY"),
         },
         {
             name: "NEXT_PUBLIC_GROPQ_API_KEY",
-            apiKey: getApiKeyFromStorage("NEXT_PUBLIC_GROPQ_API_KEY") || "",
+            apiKey: getKeyFromStorage("NEXT_PUBLIC_GROPQ_API_KEY"),
         },
     ]);
+
+    /* Sync the apiKeys state changes with the storage */
+    useEffect(() => {
+        sessionStorage.setItem("API_KEYS", JSON.stringify(apiKeys));
+    }, [apiKeys]);
 
     const setApiKeys = (
         keys: Array<{
@@ -87,20 +56,8 @@ export const useApiKeys = () => {
         setApiKeysState(keys);
     };
 
-    const addApiKey = (props: ApiKeyProps) => {
-        addApiKeyToStorage(props);
-        setApiKeysState(getApiKeysFromStorage());
-    };
-
-    const removeApiKey = (props: ApiKeyProps) => {
-        removeApiKeyFromStorage(props);
-        setApiKeysState(getApiKeysFromStorage());
-    };
-
     return {
         apiKeys,
-        addApiKey,
-        removeApiKey,
         setApiKeys,
     };
 };
--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,5 +1,3 @@
 export { insertUserPrompt, go, createNewModelData } from "./modelProcessing";
 
-export { addApiKey, removeApiKey, getApiKeys, getApiKey } from "./functions";
-
 //Todo: replace all private api keys with sessions storage keys
