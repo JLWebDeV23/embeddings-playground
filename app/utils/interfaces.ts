8fd6806148b6e285101d0d6994579f94cdff22e5
Mathias ROSA <mat.ros@outlook.fr>
2024-06-26 17:14:10 +1000

    broken : update interfaces

--- app/utils/interfaces.ts
+++ app/utils/interfaces.ts
@@ -5,69 +5,69 @@ import Anthropic from "@anthropic-ai/sdk";
 // import LlamaAI from "llamaai";
 
 export interface TreeNodeInterface {
-  id: number;
-  name: string;
-  // parent: OpenAI.ChatCompletion.Choice | null;
-  children?: TreeNodeInterface[];
+    id: number;
+    name: string;
+    // parent: OpenAI.ChatCompletion.Choice | null;
+    children?: TreeNodeInterface[];
 }
 
 export interface LogProbTreeNode {
-  token: string | null;
-  id: number;
-  logProbs: OpenAI.Chat.Completions.ChatCompletion.Choice;
-  children: LogProbTreeNode[] | null;
+    token: string | null;
+    id: number;
+    logProbs: OpenAI.Chat.Completions.ChatCompletion.Choice;
+    children: LogProbTreeNode[] | null;
 }
 
 export interface modelSDK {
-  openai: OpenAI;
-  qdrant: QdrantClient;
-  mistral: MistralClient;
-  anthropic: Anthropic;
-  // llamaai: LlamaAI;
+    openai: OpenAI;
+    qdrant: QdrantClient;
+    mistral: MistralClient;
+    anthropic: Anthropic;
+    // llamaai: LlamaAI;
 }
 
 // Define the shape of the card data
 export interface CardData {
-  x: string;
-  y: string;
+    x: string;
+    y: string;
 }
 
 export interface Message {
-  role: string;
-  content: string;
-  score?: number;
+    role: string;
+    content: string;
+    score?: number;
 }
 
 export interface ModelData {
-  model: string;
-  subModel: string;
-  messages: Message[];
-  locked: boolean;
+    model: string;
+    subModel: string;
+    messages: Message[];
+    locked: boolean;
+    apiKeys: ApiKey[];
 }
 
 export interface Model {
-  model: string;
-  subModel: string;
+    model: string;
+    subModel: string;
 }
 
 export interface ApiKey {
-  key: string;
-  name: string;
-  apiKey: string;
+    name: string;
+    apiKey: string;
 }
 [];
 
 export interface StringInterpolation {
-  key: number;
-  variable: string;
-  field: string;
+    key: number;
+    variable: string;
+    field: string;
 }
 
 export interface StringInterpolations {
-  list: StringInterpolation[];
+    list: StringInterpolation[];
 }
 
 export interface ApiKeyProps {
-  name: string;
-  apiKey: string;
+    name: string;
+    apiKey: string;
 }
