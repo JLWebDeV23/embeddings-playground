034dda59cf8839b6974bc15487115fbe9ddca441
Mathias ROSA <mat.ros@outlook.fr>
2024-06-27 16:09:47 +1000

    Build

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import Version_2_0 from "./components/Version_2_0";
 import { NextUIProvider } from "@nextui-org/react";
 import ModelDataProvider from "./providers/ModelDataProvider";
 import SystemMessageProvider from "@/app/providers/SystemMessageProvider";
-import ModalInfoProvider from "@/app/providers/ModalInfoProvider";
+
+import dynamic from "next/dynamic";
+
+const ModalInfoProvider = dynamic(
+    () => import("@/app/providers/ModalInfoProvider"),
+    {
+        ssr: false,
+    }
+);
 
 export default function Home() {
     return (
