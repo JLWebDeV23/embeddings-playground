7ebe4077629eb5da84a576374cd6ed935249511a
Mathias ROSA <mat.ros@outlook.fr>
2024-06-28 18:13:11 +1000

    Buy me a coffe logo

--- app/components/Header/BuyMeACoffee/BuyMeACoffee.tsx
+++ app/components/Header/BuyMeACoffee/BuyMeACoffee.tsx
@@ -1,16 +1,37 @@
+import { Button, Link } from "@nextui-org/react";
+import BuyMeACoffeLogoWhite from "@/app/assets/bmc-full-logo-white.svg";
+import BuyMeACoffeLogoAllWhite from "@/app/assets/bmc-full-logo-all-white.svg";
+import BuyMeACoffeLogo from "@/app/assets/bmc-full-logo.svg";
 import Image from "next/image";
+import { useState } from "react";
+import { useTheme } from "next-themes";
 
 const BuyMeACoffee = () => {
+    const [isHovered, setIsHovered] = useState(false);
+    const { theme } = useTheme();
+
+    const ThemedLogo =
+        theme === "dark" ? BuyMeACoffeLogoAllWhite : BuyMeACoffeLogo;
+
+    const currentLogo = isHovered ? BuyMeACoffeLogoWhite : ThemedLogo;
+
     return (
-        <a href="https://www.buymeacoffee.com/JLiou" target="_blank">
+        <Button
+            variant="bordered"
+            className="bg-background hover:bg-[#fd0] hover:border-foreground"
+            size="lg"
+            as={Link}
+            href="https://www.buymeacoffee.com/JLiou"
+            target="_blank"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <Image
-                src="/buyMeACoffee.png"
-                width={217}
-                height={60}
-                className="h-12 w-auto hover:scale-105 transition-all duration-300"
+                src={currentLogo}
                 alt="Buy Me A Coffee"
+                className="h-full w-auto p-2"
             />
-        </a>
+        </Button>
     );
 };
 
