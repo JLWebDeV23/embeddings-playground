8413515ef5907e724556a058cb0759a049931e66
Mathias ROSA <mat.ros@outlook.fr>
2024-07-03 10:56:54 +1000

    add points

--- app/rag2/page.tsx
+++ app/rag2/page.tsx
@@ -4,7 +4,143 @@ import SystemMessageBox from "../components/SystemMessageBox";
 import ModelSelector from "../components/ModelSelector";
 import useModelData from "../hooks/useModelData";
 import { Model } from "../utils/interfaces";
-import { Autocomplete, AutocompleteItem, Button } from "@nextui-org/react";
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    Button,
+    useDisclosure,
+    Tabs,
+    Tab,
+    Textarea,
+    Spacer,
+    Input,
+    ScrollShadow,
+} from "@nextui-org/react";
+import { useState } from "react";
+import {
+    collectionExists,
+    createCollection,
+    getCollectionsList,
+    upsertPoints,
+} from "../utils/collection";
+
+function AddSourceTab({
+    onAddSource,
+}: {
+    onAddSource: (name: string, source: string) => void;
+}) {
+    const [name, setName] = useState<string>("");
+    const [source, setSource] = useState("");
+    return (
+        <>
+            <Input
+                placeholder="Name"
+                value={name}
+                onValueChange={(value) => setName(value)}
+            />
+            <Textarea
+                placeholder="Enter the source"
+                className="w-full"
+                minRows={7}
+                maxRows={7}
+                value={source}
+                onValueChange={(value) => setSource(value)}
+            />
+            <Button
+                variant="flat"
+                className="w-fit self-end"
+                isDisabled={!name || !source}
+                onPress={() => {
+                    onAddSource(name, source);
+                }}
+            >
+                Add
+            </Button>
+        </>
+    );
+}
+
+function SourcesModal() {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [collectionList, setCollectionList] = useState<string[]>([]);
+
+    getCollectionsList().then((collections) => {
+        setCollectionList(collections.map((collection) => collection.name));
+    });
+
+    return (
+        <>
+            <Button size="sm" onPress={onOpen} variant="flat">
+                Sources
+            </Button>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="2xl">
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">
+                                Sources
+                            </ModalHeader>
+                            <ModalBody className="w-full">
+                                <Tabs aria-label="Options" isVertical>
+                                    <Tab
+                                        title="Add a new source"
+                                        key={0}
+                                        className="flex flex-col w-full gap-4"
+                                    >
+                                        <AddSourceTab
+                                            onAddSource={async (
+                                                name: string,
+                                                source: string
+                                            ) => {
+                                                const exist =
+                                                    await collectionExists(
+                                                        name
+                                                    );
+
+                                                if (exist) {
+                                                    console.log(
+                                                        "erase old data"
+                                                    );
+                                                }
+                                                createCollection(name);
+                                                upsertPoints(name, source);
+                                                setCollectionList([
+                                                    ...collectionList,
+                                                    name,
+                                                ]);
+                                            }}
+                                        />
+                                    </Tab>
+                                    {collectionList.map((collection, index) => (
+                                        <Tab
+                                            title={collection}
+                                            key={index + 1}
+                                            className="flex flex-col w-full gap-4"
+                                        >
+                                            <ScrollShadow className="h-40">
+                                                {collection}
+                                            </ScrollShadow>
+                                            <Button
+                                                className="w-fit self-end"
+                                                color="primary"
+                                                onPress={onClose}
+                                            >
+                                                Select
+                                            </Button>
+                                        </Tab>
+                                    ))}
+                                </Tabs>
+                            </ModalBody>
+                            <Spacer />
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
 
 function RagInputFooter() {
     const { models, handleModelsAction } = useModelData();
@@ -23,24 +159,8 @@ function RagInputFooter() {
                 size="sm"
                 variant="flat"
             />
-            <Autocomplete
-                aria-label="source"
-                placeholder="Select a source"
-                className="w-full max-w-64"
-                size="sm"
-                variant="flat"
-            >
-                <AutocompleteItem key="source1" value="source1">
-                    Source 1
-                </AutocompleteItem>
-            </Autocomplete>
-            <Button
-                size="sm"
-                onPress={() => alert("to be implemented")}
-                variant="flat"
-            >
-                Add a source
-            </Button>
+
+            <SourcesModal />
         </>
     );
 }
