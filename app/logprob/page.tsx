5fff41bfa3eeccda334de7a5655868e6ccf0f1d7
Mathias ROSA <mat.ros@outlook.fr>
2024-07-04 11:58:16 +1000

    working manuallly

--- app/logprob/page.tsx
+++ app/logprob/page.tsx
@@ -1,53 +1,61 @@
 "use client";
+import { Button } from "@nextui-org/react";
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
-type NodeProps = {
-  history: string[];
-  token: string;
-  children?: NodeProps[];
+
+type NodeAttributes = {
+    history: string[];
+    token: string;
 };
 
-type NodeElement = {
-  history: string[];
-  token: string;
-  children: NodeElement[];
-};
+type NodeProps = NodeAttributes & React.PropsWithChildren<{}>;
 
-const Node = ({ history, token, children }: NodeProps): NodeElement => {
-  const [childrenTokens, setChildrenTokens] = React.useState<NodeProps[]>([]);
+const Node = ({ history, token, children }: NodeProps) => {
+    const [childrenAttributes, setChildrenAttributes] = React.useState<
+        NodeAttributes[]
+    >([]);
 
-  const handleClick = () => {
-    alert(history.join("") + token);
-    setChildrenTokens((prev) => {
-      return [
-        ...prev,
-        <Node
-          key={uuidv4()}
-          history={history.concat(token)}
-          token="A good day"
-        ></Node>,
-      ];
-    });
-  };
+    const handleClick = () => {
+        alert(history.join("") + token);
+        console.log(history);
+        setChildrenAttributes((prev) => {
+            return [
+                ...prev,
+                {
+                    history: history,
+                    token: token,
+                },
+            ];
+        });
+    };
 
-  return (
-    <div>
-      <div>{token}</div>
-      <button onClick={handleClick}>Click</button>
-      {children &&
-        children.map((child, index) => (
-          <Node history={history.concat(token)} token={child.token} key={index}>
-            {child.children || []}
-          </Node>
-        ))}
-    </div>
-  );
+    return (
+        <div className="flex">
+            <button onClick={handleClick}>{token}</button>
+            {children}
+            {childrenAttributes.map((childrenAttribute, index) => {
+                return (
+                    <Node
+                        key={uuidv4()}
+                        history={[...history, token]}
+                        token={childrenAttribute.token}
+                    ></Node>
+                );
+            })}
+        </div>
+    );
 };
 
 const Page = () => {
-  const nodes = <Node history={[]} token="A good day"></Node>;
+    const nodes = (
+        <Node history={[]} token="A">
+            <Node history={["A"]} token="&nbsp;good">
+                <Node history={["A", "&nbsp;good"]} token="&nbsp;day"></Node>
+            </Node>
+        </Node>
+    );
 
-  return <div>{nodes}</div>;
+    return <div>{nodes}</div>;
 };
 
 export default Page;
