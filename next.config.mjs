0ebcb776491250d296f035a2a61fb613b8c7d184
Joey <127422383+JLWebDeV23@users.noreply.github.com>
2024-07-08 15:35:28 +1000

    configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
-  // distDir: 'dist',
-  rewrites: async () => [
-    {
-      source: "/anthropic/:path*",
-      destination: "https://api.anthropic.com/:path*",
-    },
-  ],
+  output: "export",
+  distDir: "dist",
+  // rewrites: async () => [
+  //   {
+  //     source: "/anthropic/:path*",
+  //     destination: "https://api.anthropic.com/:path*",
+  //   },
+  // ],
 };
 
 export default nextConfig;
